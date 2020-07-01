9fceab353bfe1def5a4a76699f0bff9de823abd7
liqiang2020 <liqiang64@huawei.com>
2020-07-01 20:01:01 +0800

    add prefetch_tuning.h. 架构调整，新增.h文件

---
+++ prefetch_tuning.h
@@ -0,0 +1,29 @@
+#ifndef __PREFETCH_TUNING__
+#define __PREFETCH_TUNING__
+
+#define ENABLE 1
+#define DISABLE 0
+#define CACHE_READUNIQ_OFFSET 40
+#define CACHE_READUNIQ_CTRL (1L << CACHE_READUNIQ_OFFSET)
+
+#ifdef CONFIG_ARCH_HISI
+typedef struct {
+	long cpuprefctrl_el1;
+	long adps_lld_ddr_el1;
+	long adpp_l1v_mop_el1;
+	long adps_lld_l3_el1;
+} cfg_t;
+#else
+typedef long cfg_t;
+#endif
+
+extern void set_prefetch(void* dummy);
+extern void get_prefetch(void* dummy);
+extern void cache_set(void *dummy);
+extern void cache_get(void *dummy);
+extern void reset_prefetch(void* dummy);
+
+extern int prefetch_policy_num(void);
+extern cfg_t *prefetch_policy(int policy);
+
+#endif
