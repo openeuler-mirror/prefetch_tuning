d177e7cb46f836c19eb38be9a165ee0ed099bc81
liqiang <liqiang64@huawei.com>
2020-07-03 16:47:34 +0800

    Modify display information of read_unique

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -22,37 +22,37 @@ do
         cat /sys/class/misc/prefetch/policy | grep $GREP
 done
 
-echo "Set read_unique to 0, (exp:close,15):"
+echo "Set read_unique to 0, (exp:0,15):"
 echo 0 > /sys/class/misc/prefetch/read_unique
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
-echo "Set policy to 8, (exp:close,8):"
+echo "Set policy to 8, (exp:0,8):"
 echo 8 > /sys/class/misc/prefetch/policy
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
 
-echo "Set read_unique to 1, (exp:open,8):"
+echo "Set read_unique to 1, (exp:1,8):"
 echo 1 > /sys/class/misc/prefetch/read_unique
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
 
-echo "Set policy to 10, (exp:open,10):"
+echo "Set policy to 10, (exp:1,10):"
 echo 10 > /sys/class/misc/prefetch/policy
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
 
 
-echo "Set policy to 5, (exp:open, 5):"
+echo "Set policy to 5, (exp:1, 5):"
 echo 5 > /sys/class/misc/prefetch/policy
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
 
-echo "Set read_unique to 0, (close,5):"
+echo "Set read_unique to 0, (0,5):"
 echo 0 > /sys/class/misc/prefetch/read_unique
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
 
-echo "Set read_unique to 1, (open,5):"
+echo "Set read_unique to 1, (1,5):"
 echo 1 > /sys/class/misc/prefetch/read_unique
 cat /sys/class/misc/prefetch/read_unique | grep $GREP
 cat /sys/class/misc/prefetch/policy | grep $GREP
